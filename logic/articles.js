f8f59001c5cbed28cf8afb661184c2b4447573de
smdsbz <694066681@qq.com>
2018-04-24 21:01:44 +0800

    added getArticles and list

--- logic/articles.js
+++ logic/articles.js
@@ -18,23 +18,65 @@ const Articles = sequelize.import('../models/articles.js'); // import model
 // Define user logic level methods
 module.exports = {
 
-  queryByTime: (startTime, endTime, limitCount) => {
-    if (!startTime) {
-      return Articles.findOne({ limit: 25 });
-    }
-    if (!endTime) {     // looking for specific article
-      return Articles.findOne({ where: { publish_time: startTime } });
-    } else {            // looking for multiple articles
+  getArticle: (params) => {
+    /**
+     * query `Dian_officialsite`.`articles` via time
+     *
+     * Args:
+     * +--------------+-------------------------------------+-----------+
+     * | Field        | Description                         | Required? |
+     * +--------------+-------------------------------------+-----------|
+     * | publish_time | the article's publish_time querying | yes       |
+     * +--------------+-------------------------------------+-----------+
+     *
+     * Return:
+     *     Record in database table.
+     */
+    if (params.publish_time) {
       return Articles.findOne({
         where: {
-          publish_time: {
-            [Op.gte]: new Date(startTime),
-            [Op.lte]: new Date(endTime)
-          }
-        },
-        limit: limitCount || 25
+          publish_time: new Date(params.publish_time)
+        }
       });
     }
+    return null;
+  },
+
+  list: (params) => {
+    /**
+     * get list of articles in `Dian_officialsite`.`articles`
+     *
+     * Args:
+     * +------------+---------------------------+-----------+
+     * | Field      | Description               | Required? |
+     * +------------+---------------------------+-----------|
+     * | limit      | count of articles to show | no        |
+     * | start_time | starting time             | no        |
+     * | end_time   | ending time               | no        |
+     * +------------+---------------------------+-----------+
+     *
+     * Return:
+     *     An `Array` of records.
+     */
+    if (params.start_time) {
+      params.start_time = new Date(params.start_time);
+    } else {
+      params.start_time = new Date('1970-01-01');   // should be early enough
+    }
+    if (params.end_time) {
+      params.end_time = new Date(params.end_time);
+    } else {
+      params.end_time = new Date();     // now is the latest you can get
+    }
+    return Articles.findAll({
+      where: {
+        publish_time: {
+          [Op.gte]: params.start_time,
+          [Op.lte]: params.end_time
+        }
+      },
+      limit: params.limit || 25
+    });
   },
 
   create: (record) => {
@@ -58,6 +100,7 @@ module.exports = {
     });
   }
 
+
 };
 
 
@@ -65,7 +108,7 @@ module.exports = {
 let test = true;
 if (test) {
   module.exports.create({
-      title: 'ANOTHER BREAKING NEWS!!!',
+      title: 'BREAKING NEWS!!!',
       publish_time: '1970-01-01',
       content: 'A big news made by Mr.口-口'
     })
@@ -82,9 +125,22 @@ if (test) {
       console.log(`Error occured: ${err.msg}`);
     });
 
-  module.exports.queryByTime()
+  module.exports.getArticle({publish_time: '1970-01-01'})
     .then((data) => {
-      console.log(`Found ${data.title} inserted at ${data.publish_time}`);
+      if (data) {
+        console.log(`Found ${data.title} in records!`);
+      } else {
+        console.log(`No data found!`);
+      }
+    });
+
+  module.exports.list({start_time: '1970-01-01'})
+    .then((data) => {
+      if (data) {
+        console.log(`Found ${data.length} records!`);
+      } else {
+        console.log(`No data found!`);
+      }
     });
 }
 
