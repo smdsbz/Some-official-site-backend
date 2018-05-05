74aa74df8d724fd27940d5aae5b7e38c79b7ff06
smdsbz <694066681@qq.com>
2018-05-05 16:39:35 +0800

    CR required

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var bodyParser = require('body-parser');
+// var multer = require('multer');
+// var upload = multer();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles.js');
 
 var app = express();
 
@@ -15,12 +19,13 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/article_list', articlesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
